2b74040e95e00a4661667cd72d9728083143d6df
Samuel Green <SGreen@merkleinc.com>
2022-01-31 20:35:38 +0000

    AMEX POC

--- models/load_data_to_snowflake_stage.sql
+++ models/load_data_to_snowflake_stage.sql
@@ -1,6 +1,6 @@
 COPY INTO {{ var("SNOWFLAKESTAGEOUTBOUND") }}/{{ var("SNOWFLAKETBLNAME") }}_audience_m1.tsv.gz
 FROM (select distinct IND_ID
-from {{ var("SNOWFLAKEDB") }}.{{ var("SNOWFLAKESCHEMA2") }}.{{ var("SNOWFLAKETBLNAME") }}_AUDIENCE_M1)
+from {{ref('convert_cmid_to_M1ID')}}
 file_format=(format_name='{{ var("SNOWFLAKEDB") }}.{{ var("SNOWFLAKESCHEMA2") }}.{{ var("FILEFORMATOUTBOUND") }}' file_extension=tsv COMPRESSION = 'GZIP') 
 header=true 
 single=true
--- models/load_from_snowflake_stage.sql
+++
@@ -1,8 +0,0 @@
-COPY INTO {{ var("SNOWFLAKEDB") }}.{{ var("SNOWFLAKESCHEMA") }}.{{ var("SNOWFLAKETBLNAME") }} 
-FROM ( 
-    select $1
-        , '{{ var("LOADDATE") }}'
-    from '{{ var("SNOWFLAKESTAGEINBOUND") }}/{{ var("FILENAME") }}'
-    )
-FILE_FORMAT = '{{ var("SNOWFLAKEDB") }}.{{ var("SNOWFLAKESCHEMA") }}.{{ var("FILEFORMATINBOUND") }}'
-ON_ERROR = 'SKIP_FILE';
