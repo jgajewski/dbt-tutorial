1ec1e7fca49f5ca49a6cf1faf7a811507d3a9947
Samuel Green <SGreen@merkleinc.com>
2022-01-31 18:26:49 +0000

    AMEX POC

--- models/convert_cmid_to_M1ID.sql
+++ models/convert_cmid_to_M1ID.sql
@@ -1,5 +1,6 @@
 --create or replace table {{ var("SNOWFLAKEDB") }}.{{ var("SNOWFLAKESCHEMA2") }}.{{ var("SNOWFLAKETBLNAME") }}_AUDIENCE_M1 as 
 select distinct b.INDIVIDUAL_ID as IND_ID
-from  {{ source('SHARED_QUBOLE_AUDIENCES', 'N_bcp_') }}{{ var("SNOWFLAKETBLNAME") }}  a
+from  {{ source('AMEX_INBOUND', 'M1_AUDIENCE_SOURCE') }}  a
 inner join {{ref('c3_compliant_M1_DSIID_CMID_DSXXXX')}} b
 on a.CMID = b.CMID
+where right(METADATA$FILENAME, length('{{ var("FILENAME") }}')) = '{{ var("FILENAME") }}'
